291a88a10c6a7b9c3e2fef8e04c826057297077c
s-panicker <sahilanil007@gmail.com>
2025-02-27 18:56:25 +0100

    Dani's files

--- js/main.js
+++ js/main.js
@@ -34,3 +34,36 @@ function switchPages (key) {
         break;
     }
 }
+
+    const header = document.getElementById('header');
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY > 50) {
+            header.classList.add('bg-transparent');
+            header.classList.remove('bg-[#d3c1a9]');
+        } else {
+            header.classList.add('bg-[#d3c1a9]');
+            header.classList.remove('bg-transparent');
+        }
+    });
+
+        window.addEventListener("scroll", function() {
+            const header = document.querySelector("header");
+            if (window.scrollY > 50) {
+                header.classList.add("bg-white", "shadow-lg");
+                header.classList.remove("bg-[#d3c1a9]]");
+            } else {
+                header.classList.add("bg-[#d3c1a9]");
+                header.classList.remove("bg-white", "shadow-lg");
+            }
+        });
+ 
+    document.addEventListener("DOMContentLoaded", function () {
+        const menuButton = document.querySelector("#mobile-menu-button");
+        const menu = document.querySelector("#mobile-menu");
+
+        menuButton.addEventListener("click", function () {
+            menu.classList.toggle("hidden");
+        });
+    });
+
